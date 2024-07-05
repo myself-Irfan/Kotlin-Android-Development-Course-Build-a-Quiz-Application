8fa9c2f052308c8a99e234bf8fbf6d84d5d09357
Irfan <ahmed.1995.irfan@gmail.com>
2024-07-05 23:42:34 +0600

    reorganization

---
+++ src/CollectionTypes.kt
@@ -0,0 +1,95 @@
+/*
+Demonstrates different collection types
+ */
+
+fun main() {
+    setType()
+}
+
+fun mapOp() {
+    val nums = setOf(1, 2, 3, 4, 5)
+    println("Set: $nums")
+    nums.map {if (it%2 == 0) println("$it is Even") else println("$it is Odd")}
+
+    // returns a new List instead of changing the og val
+    val numsMap = nums.map {it * 10}
+    println("numsMap: $numsMap")
+    // mapIndexedNotNull iterates over each element with index and exclude null
+    // null is set by the logic in the body
+    println(numsMap.mapIndexedNotNull{ index, value -> if (index == 0) null else index + value})
+
+    // declaring a map with keys and values
+    val numsMap2 = mapOf("k1" to 1, "k2" to 2, "k3" to 3)
+    // assigning numsMap2 to a val with uppercase keys
+    var m1 = numsMap2.mapKeys { it.key.uppercase() }
+    m1 = m1.mapValues { it.key.length + it.value }
+    println(m1)
+}
+
+
+fun mapType() {
+    // immutable map
+    val map1 = mapOf(1 to "Maria", 2 to "Alex", 3 to "Asif")
+    println(map1)
+
+    // mutable map
+    val map2 = mutableMapOf(0 to "Irfan", 1 to "Amaterasu", 2 to "Asif", 3 to "Susanoo")
+    map2.remove(2) // remove using key
+    map2[2] = "Vlad"
+    map2.forEach{index, value -> println("Index: $index\tValue: $value")}
+}
+
+
+fun listType() {
+    // immutable list
+    val lst1 = listOf("Name1", "Name2", "Name3")
+    println(lst1)
+
+    // mutable list
+    val lst2 = mutableListOf("Name1", "Name2", "Name3")
+    println("Before $lst2")
+    lst2.removeAt(1) // remove using index
+    lst2.remove("Name3") // remove using element
+    lst2.add("Name4") // append element
+    println("After $lst2")
+}
+
+fun setType() {
+    // Can not contain duplicates, nor can we make changes
+    val set1 = setOf(1, 2, 3, 4, 5)
+    println(set1)
+
+    val set2 = setOf("Name1", "Name2", "Name3")
+    println(set2)
+}
+
+
+fun dataClassDemo() {
+    val usr1 = User("Name1")
+    val usr2 = User(" name2")
+    val usr3 = User("NaMe1")
+
+    val nameSet = setOf(usr1, usr2, usr3)
+    nameSet.forEach{ println(it.name) }
+
+    val dUsr1 = DUser("Name1")
+    val dUsr2 = DUser(" name2")
+    val dUsr3 = DUser("NaMe1")
+
+    val nameSet2 = setOf(dUsr1, dUsr2, dUsr3)
+    nameSet2.forEach{ println(it.name) }
+}
+
+
+data class DUser(var name: String) {
+    init {
+        name = name.trim().lowercase()
+    }
+}
+
+
+class User(var name: String) {
+    init {
+        name = name.trim().lowercase()
+    }
+}
--- src/Main.kt
+++ src/Main.kt
@@ -3,4 +3,13 @@ import kotlin.reflect.typeOf
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
+fun main() {
+    val num = listOf("one", "two", "three", "four")
+    val longThan3 = num.filter {it.length > 3}
+    println(longThan3)
 
+    // declaring a 
+    val numMap = mapOf("K1" to 1, "K2" to 2, "K3" to 3, "K4" to 4, "K101" to 101)
+    val filteredMap = numMap.filter { it.key.endsWith("1") && it.value > 100}
+    println(filteredMap)
+}
