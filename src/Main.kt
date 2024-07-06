ff6e57997db471468c5be4b29c7586f71e8d0fde
Irfan <ahmed.1995.irfan@gmail.com>
2024-07-06 18:50:40 +0600

    "Reorganized"

--- src/Main.kt
+++ src/Main.kt
@@ -1,19 +1,3 @@
-import kotlin.reflect.typeOf
-
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
-fun main() {
-    val num = listOf("one", "two", "three", "four")
-    val longThan3 = num.filter {it.length > 3}
-    println(longThan3)
-
-    // declaring a map of keys and values
-    val numMap = mapOf("K1" to 1, "K2" to 2, "K3" to 3, "K4" to 4, "K101" to 101)
-    // filtering by value and assigning to a val
-    val filteredMap = numMap.filter { it.key.endsWith("1") && it.value > 100}
-    println(filteredMap)
-    // filtering by index or index and value
-    val filteredIndex = num.filterIndexed {index, value -> (index != 0) && (value.length < 5)}
-    println(filteredIndex)
-}
--- src/TransformDemo.kt
+++ src/TransformDemo.kt
@@ -1,3 +1,69 @@
+fun partitionDemo() {
+    /*
+    Demonstrates partition methods
+     */
+    val num = (1..10).toList()
+    // returns a pair of list from list, first list defined by logic and second list the rest
+    val (even, odd) = num.partition {it%2==0}
+    println(even)
+    println(odd)
+}
+
+
+fun filterDemo() {
+    /*
+    Demonstrates filter methods
+     */
+    val num = listOf("one", "two", "three", "four")
+    val longThan3 = num.filter {it.length > 3} // filter by value length
+    println(longThan3)
+
+    // declaring a map of keys and values
+    val numMap = mapOf("K1" to 1, "K2" to 2, "K3" to 3, "K4" to 4, "K101" to 101)
+    // filtering by key/value and assigning to a val
+    val filteredMap = numMap.filter { it.key.endsWith("1") && it.value > 100}
+    println(filteredMap)
+    // filtering by index or index and value
+    val filteredIndex = num.filterIndexed {index, value -> (index != 0) && (value.length < 5)}
+    println(filteredIndex)
+    val filteredNot = num.filterNot { it.length <= 3 } // opposite of logic will be returned similar to not
+    println(filteredNot)
+
+    val mixdLi = listOf(1, 2, "One", "two", '1', "2", 'A', false)
+    // filter col by dtype
+    mixdLi.filterIsInstance<Char>().forEach {
+        println("Instance filtered: $it")
+    }
+}
+
+
+fun unzipDemo() {
+    val numPair = listOf("red" to 1, "blue" to 2, "green" to 3, "yellow" to 4, "pink" to 5)
+    println("Init: $numPair")
+    println("Unzipped: ${numPair.unzip()}")
+}
+
+
+fun zipDemo() {
+    /*
+    Zip is used to combine two collections into a pair
+    */
+    val col = listOf("red", "brown", "grey")
+    val animals = listOf("fox", "bear", "wolf")
+
+    // zip syntax
+    println(col.zip(animals))
+//    println(col zip animals)
+
+    // returning a zipped string defined in the body
+    val colStatement = col.zip(animals) { col, animal ->
+        "The ${animal.replaceFirstChar { it.uppercase() }} is $col"
+    }
+    println(colStatement)
+//    colStatement.forEach { println(it) }
+}
+
+
 fun flattenDemo() {
     // declaring a list of sets
     val numSets = listOf(setOf(1, 2, 3), setOf(4, 5, 6), setOf(7, 8 ,9))
@@ -48,5 +114,4 @@ fun strRepresentation() {
     println(nums)
     // joining only 15 elements and replace rest with specified in truncated
     println(nums.joinToString(limit= 15, truncated = "<..>"))
-
 }
